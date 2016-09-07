1b81cc847f4408659f8990bff8460ebab13591f1
Sattar Hummatli <settarxan@gmail.com>
2016-09-07 17:03:31 +0400

    Deleted unused files

--- MAHAndroidUpdater/mah-android-updater/src/main/java/com/mobapphome/mahandroidupdater/tools/LocaleUpdater.java
+++ MAHAndroidUpdater/mah-android-updater/src/main/java/com/mobapphome/mahandroidupdater/tools/LocaleUpdater.java
@@ -6,14 +6,6 @@ import android.content.res.Resources;
 
 import java.util.Locale;
 
-/**
- * This class is used to change your application locale and persist this change for the next time
- * that your app is going to be used.
- * <p/>
- * You can also change the locale of your application on the fly by using the setLocale method.
- * <p/>
- * Created by gunhansancar on 07/10/15.
- */
 public class LocaleUpdater {
     private static void updateLocale(Context context, String language) {
         Locale locale = new Locale(language);
